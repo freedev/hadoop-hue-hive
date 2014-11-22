131d43ca8e47766922fae83021f2196e31f40032
Vincenzo D'Amore <v.damore@gmail.com>
2014-11-22 04:24:15 +0100

    provisioning hadoop

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default['hadoop-hue-hive']['user'] = 'hduser'
 default['hadoop-hue-hive']['group'] = 'hadoop'
-default['hadoop-hue-hive']['url_src'] = "http://apache.fis.uniroma2.it/hadoop/common/hadoop-2.5.1/hadoop-2.5.1.tar.gz"
+
+default['hadoop-hue-hive']['url_src'] = "http://apache.fis.uniroma2.it/hadoop/common/hadoop-2.5.2/hadoop-2.5.2.tar.gz"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -11,27 +11,30 @@ require 'uri'
 
 hadoop_user = node['hadoop-hue-hive']['user']
 hadoop_group = node['hadoop-hue-hive']['group']
-user_home = "/home/#{hadoop_user}"
+hadoop_user_home = "/home/#{hadoop_user}"
 hadoop_url = node['hadoop-hue-hive']['url_src']
 hadoop_uri = URI.parse(hadoop_url)
 hadoop_filename = File.basename(hadoop_uri.path)
 hadoop_basename = File.basename(hadoop_uri.path, '.tar.gz')
 basedir = '/usr/local'
 hadoop_basedir = "#{basedir}/#{hadoop_basename}"
+hadoop_home = "#{basedir}/hadoop"
 
 group hadoop_group
 
 user hadoop_user do
   group hadoop_group
   shell '/bin/bash'
-  home user_home
+  home hadoop_user_home
+  supports :manage_home => true
   password '$1$RF0eUP9s$r.H4rcWvQ9zc5REJydT2c/'
   action :create
 end
 
+puts "Adding localhost to #{hadoop_user_home}/.ssh/known_hosts"
+
 ssh_known_hosts_entry 'localhost'
 
-puts "Downloading #{hadoop_url} into #{hadoop_basedir}"
 
 remote_file "/tmp/#{hadoop_filename}" do
   source hadoop_url
@@ -48,5 +51,60 @@ end
 
 directory hadoop_basedir do 
   owner hadoop_user
-  group
+  group hadoop_group
 end
+
+execute "ln-s-#{hadoop_basename}" do
+  user "root"
+  command "ln -s #{hadoop_basedir} #{hadoop_home}"
+  action :run
+  cwd basedir
+  returns 0
+  not_if { ::File.exists?(hadoop_home) }
+end
+
+template "#{hadoop_user_home}/hadoop_bashrc" do
+  source "hadoop_bashrc.erb"
+  action :create
+end
+
+execute "modify-bashrc" do
+  user "root"
+  command "cat #{hadoop_user_home}/hadoop_bashrc >> #{hadoop_user_home}/.bashrc"
+  action :run
+  cwd basedir
+  returns 0
+  not_if "grep -q HADOOP_HOME #{hadoop_user_home}/.bashrc"
+end
+
+directory "/app/hadoop/tmp" do
+  owner hadoop_user
+  group hadoop_group
+  mode '0775'
+  action :create
+  recursive true
+end
+
+template "#{hadoop_home}/etc/hadoop/core-site.xml" do
+  source "core-site.xml.erb"
+  action :create
+end
+
+template "#{hadoop_home}/etc/hadoop/mapred-site.xml" do
+  source "mapred-site.xml.erb"
+  action :create
+end
+
+template "#{hadoop_home}/etc/hadoop/hdfs-site.xml" do
+  source "hdfs-site.xml.erb"
+  action :create
+end
+
+execute "modify-bashrc" do
+  user "root"
+  command "#{hadoop_home}/bin/hadoop namenode -format"
+  action :run
+  cwd basedir
+  returns 0
+end
+
